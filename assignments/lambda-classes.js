ce5bb475df4e31bb96ecf1b5f84fdd011455e62b
Krishan Nattar <krishan.nattar@gmail.com>
2019-07-25 08:54:13 -0700

    Reduced interval times and fixed clearInterval glitch

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,100 +1,112 @@
 // CODE here for your Lambda Classes
 
-class Person{
-    constructor(atts){
-        this.name = atts.name;
-        this.age = atts.age;
-        this.location = atts.location;
-    }
-    speak = function(){
-        console.log(`Hello my name is ${this.name}, I am from ${this.location}.`)
-    }
+class Person {
+  constructor(atts) {
+    this.name = atts.name;
+    this.age = atts.age;
+    this.location = atts.location;
+  }
+  speak = function() {
+    console.log(`Hello my name is ${this.name}, I am from ${this.location}.`);
+  };
 }
 
-class Instructor extends Person{
-    constructor(atts){
-        super(atts);
-        this.specialty = atts.specialty;
-        this.favLanguage = atts.favLanguage;
-        this.catchPhrase = atts.catchPhrase;
-    };
-    demo = function(subject){
-        console.log(`Today we are learning about ${subject}`);
-    };
-    grade = function(student, subject){
-        console.log(`${student.name} recieves a perfect score on ${subject}`);
-    };
+class Instructor extends Person {
+  constructor(atts) {
+    super(atts);
+    this.specialty = atts.specialty;
+    this.favLanguage = atts.favLanguage;
+    this.catchPhrase = atts.catchPhrase;
+  }
+  demo = function(subject) {
+    console.log(`Today we are learning about ${subject}`);
+  };
+  grade = function(student, subject) {
+    console.log(`${student.name} recieves a perfect score on ${subject}`);
+  };
 
-    checkAssignment = function(student){
-        let value = [1,-1];
-        let correction = Math.floor(Math.random() * 30);
-        var rand = value[Math.floor(Math.random() * value.length)];
-        correction = rand*correction;
-        student.grade += correction;
-        console.log(`${this.name} graded an assignment and made a ${correction} point correction to ${student.name}'s grade, which is now ${student.grade}`);
-    };
+  checkAssignment = function(student) {
+    let value = [1, -1];
+    let correction = Math.floor(Math.random() * 30);
+    var rand = value[Math.floor(Math.random() * value.length)];
+    correction = rand * correction;
+    student.grade += correction;
+    console.log(
+      `${
+        this.name
+      } graded an assignment and made a ${correction} point correction to ${
+        student.name
+      }'s grade, which is now ${student.grade}`
+    );
+  };
 }
 
-class Student extends Person{
-    constructor(atts){
-        super(atts);
-        this.previousBackground = atts.previousBackground;
-        this.className = atts.className;
-        this.favSubjects = atts.favSubjects;
-        this.grade = atts.grade;
-    }
-    listSubjects = function(){
-        this.favSubjects.forEach(function(item, index, array){
-            console.log(index+1, item);
-        });
-    }
-    PRAssignment = function(subject){
-        console.log(`${this.name} has submitted a PR for ${subject}`);
-    }
-    sprintChallenge = function(subject){
-        console.log(`${this.name} has begun sprint challenge on ${subject}`);
-    }
+class Student extends Person {
+  constructor(atts) {
+    super(atts);
+    this.previousBackground = atts.previousBackground;
+    this.className = atts.className;
+    this.favSubjects = atts.favSubjects;
+    this.grade = atts.grade;
+  }
+  listSubjects = function() {
+    this.favSubjects.forEach(function(item, index, array) {
+      console.log(index + 1, item);
+    });
+  };
+  PRAssignment = function(subject) {
+    console.log(`${this.name} has submitted a PR for ${subject}`);
+  };
+  sprintChallenge = function(subject) {
+    console.log(`${this.name} has begun sprint challenge on ${subject}`);
+  };
 
-    graduate = function(){
-        if(this.grade > 70){
-            console.log(`${this.name} has graduated from ${this.className}`);
-        } else {
-            console.log(`${this.name} doesn't meet the requirements for graduation. Keep studying!`);
-        }
-    };
+  graduate = function() {
+    if (this.grade > 70) {
+      console.log(`${this.name} has graduated from ${this.className}`);
+      return true;
+    } else {
+      console.log(
+        `${
+          this.name
+        } doesn't meet the requirements for graduation. Keep studying!`
+      );
+      return false;
+    }
+  };
 }
 
-class ProjectManager extends Instructor{
-    constructor(atts){
-        super(atts);
-        this.gradClassName = atts.gradClassName;
-        this.favInstructor = atts.favInstructor;
-    }
-    standUp = function(channel){
-        console.log(`${this.name} announces to ${channel}, @channel standy times!`)
-    }
-    debugsCode = function(student, subject){
-        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
-    }
+class ProjectManager extends Instructor {
+  constructor(atts) {
+    super(atts);
+    this.gradClassName = atts.gradClassName;
+    this.favInstructor = atts.favInstructor;
+  }
+  standUp = function(channel) {
+    console.log(`${this.name} announces to ${channel}, @channel standy times!`);
+  };
+  debugsCode = function(student, subject) {
+    console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+  };
 }
 
 const brit = new Instructor({
-    name: "Brit",
-    age: 30,
-    location: "Canada",
-    specialty: "HTML & CSS",
-    favLanguage: "French", 
-    catchPhrase: "Good Morning Everyone!"
+  name: "Brit",
+  age: 30,
+  location: "Canada",
+  specialty: "HTML & CSS",
+  favLanguage: "French",
+  catchPhrase: "Good Morning Everyone!"
 });
 
 let krishan = new Student({
-    name: "Krishan",
-    age: 33,
-    location: "USA",
-    previousBackground: "Self-Study",
-    className: "WEB22",
-    favSubjects: ["Javascript", "CSS", "PHP"],
-    grade: 80
+  name: "Krishan",
+  age: 33,
+  location: "USA",
+  previousBackground: "Self-Study",
+  className: "WEB22",
+  favSubjects: ["Javascript", "CSS", "PHP"],
+  grade: 80
 });
 
 brit.demo("Javascript Closures");
@@ -107,30 +119,37 @@ krishan.PRAssignment("Javascipt IV");
 krishan.sprintChallenge("Javascript Fundamentals");
 
 const shannon = new ProjectManager({
-     name: "Shannon",
-     age: 32,
-     location: "California",
-     specialty: "Javascript",
-     favLanguage: "Python",
-     catchPhrase: "How y'all doing today?",
-     gradClassName: "Web15",
-     favInstructor: "Josh"
+  name: "Shannon",
+  age: 32,
+  location: "California",
+  specialty: "Javascript",
+  favLanguage: "Python",
+  catchPhrase: "How y'all doing today?",
+  gradClassName: "Web15",
+  favInstructor: "Josh"
 });
 
 shannon.speak();
-shannon.debugsCode(krishan);
+shannon.debugsCode(krishan, "Javascript IV");
 shannon.demo("Javascript Callbacks");
 shannon.grade(krishan, "Javascript III");
 shannon.standUp("Web22_Shannon");
 
+let krishanGraduate = false;
 
+let gradeCount = 0;
 
-shannon.checkAssignment(krishan);
-brit.checkAssignment(krishan);
-krishan.graduate();
+let gradeInterval = window.setInterval(function() {
+  gradeCount++;
+  shannon.checkAssignment(krishan);
+  brit.checkAssignment(krishan);
+  krishanGraduate = krishan.graduate();
 
-while(krishan.grade <= 70){
-    shannon.checkAssignment(krishan);
-    brit.checkAssignment(krishan);
-    krishan.graduate();
-}
+  if (krishanGraduate == true) {
+    clearInterval(gradeInterval);
+  }
+  if (gradeCount == 1000) {
+    console.log(`1000 rounds have passed, code stopped`);
+    clearInterval(gradeInterval);
+  }
+}, 300);
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -9,24 +9,24 @@ Prototype Refactor
 */
 
 class GameObject {
-    constructor(obj) {
-      this.createdAt = obj.createdAt;
-      this.name = obj.name;
-      this.dimensions = obj.dimensions;
-    }
-    destroy = function(){
-      return `${this.name} was removed from the game.`;
-    }
+  constructor(obj) {
+    this.createdAt = obj.createdAt;
+    this.name = obj.name;
+    this.dimensions = obj.dimensions;
   }
+  destroy = function() {
+    return `${this.name} was removed from the game.`;
+  };
+}
 
 class CharacterStats extends GameObject {
   constructor(obj) {
     super(obj);
     this.healthPoints = obj.healthPoints;
   }
-  takeDamage = function(){
+  takeDamage = function() {
     return `${this.name} took damage`;
-  }
+  };
 }
 
 class Humanoid extends CharacterStats {
@@ -36,9 +36,9 @@ class Humanoid extends CharacterStats {
     this.weapons = obj.weapons;
     this.language = obj.language;
   }
-  greet = function(){
+  greet = function() {
     return `${this.name} offers a greeting in ${this.language}`;
-  }
+  };
 }
 
 const mage = new Humanoid({
@@ -94,81 +94,80 @@ console.log(archer.greet()); // Lilith offers a greeting in Elvish.
 console.log(mage.takeDamage()); // Bruce took damage.
 console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 
-
 // Stretch task:
 // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.
 // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
 // * Create two new objects, one a villain and one a hero and fight it out with methods!
 
-class Villain extends Humanoid{
-    constructor(obj){
-        super(obj)
+class Villain extends Humanoid {
+  constructor(obj) {
+    super(obj);
+  }
+  sneakAttack = function(obj) {
+    if (obj.healthPoints > 0) {
+      let damageAmount = Math.floor(
+        Math.random() *
+          (this.dimensions["length"] *
+            this.dimensions["width"] *
+            this.dimensions["height"])
+      );
+      obj.healthPoints -= damageAmount;
+      let randomWeapon = Math.floor(Math.random() * this.weapons.length);
+      if (obj.healthPoints > 0) {
+        console.log(
+          `${this.name} attacks ${obj.name} with ${
+            this.weapons[randomWeapon]
+          } for ${damageAmount} damage! ${obj.name} has ${
+            obj.healthPoints
+          } health points left!`
+        );
+      } else {
+        console.log(
+          `${this.name} attacks ${obj.name} with ${
+            this.weapons[randomWeapon]
+          }! ${obj.name} has no health points left!`
+        );
+        console.log(obj.destroy());
+      }
     }
-    sneakAttack = function(obj) {
-        if (obj.healthPoints > 0) {
-          let damageAmount = Math.floor(
-            Math.random() *
-              (this.dimensions["length"] *
-                this.dimensions["width"] *
-                this.dimensions["height"])
-          );
-          obj.healthPoints -= damageAmount;
-          let randomWeapon = Math.floor(Math.random() * this.weapons.length);
-          if (obj.healthPoints > 0) {
-            console.log(
-              `${this.name} attacks ${obj.name} with ${
-                this.weapons[randomWeapon]
-              } for ${damageAmount} damage! ${obj.name} has ${
-                obj.healthPoints
-              } health points left!`
-            );
-          } else {
-            console.log(
-              `${this.name} attacks ${obj.name} with ${this.weapons[randomWeapon]}! ${
-                obj.name
-              } has no health points left!`
-            );
-            console.log(obj.destroy());
-          }
-        }
-      };
+  };
 }
 
-class Hero extends Humanoid{
-    constructor(obj){
-        super(obj);
+class Hero extends Humanoid {
+  constructor(obj) {
+    super(obj);
+  }
+  justiceStrike = function(obj) {
+    if (obj.healthPoints > 0) {
+      let damageAmount = Math.floor(
+        Math.random() *
+          (this.dimensions["length"] *
+            this.dimensions["width"] *
+            this.dimensions["height"])
+      );
+
+      obj.healthPoints -= damageAmount;
+      let randomWeapon = Math.floor(Math.random() * this.weapons.length);
+      if (obj.healthPoints > 0) {
+        console.log(
+          `${this.name} attacks ${obj.name} with ${
+            this.weapons[randomWeapon]
+          } for ${damageAmount} damage! ${obj.name} has ${
+            obj.healthPoints
+          } health points left!`
+        );
+      } else {
+        console.log(
+          `${this.name} attacks ${obj.name} with ${
+            this.weapons[randomWeapon]
+          } for ${damageAmount}! ${obj.name} has ${
+            obj.healthPoints
+          } health points left!`
+        );
+        console.log(obj.destroy());
+      }
     }
-    justiceStrike = function(obj) {
-        if (obj.healthPoints > 0) {
-          let damageAmount = Math.floor(
-            Math.random() *
-              (this.dimensions["length"] *
-                this.dimensions["width"] *
-                this.dimensions["height"])
-          );
-      
-          obj.healthPoints -= damageAmount;
-          let randomWeapon = Math.floor(Math.random() * this.weapons.length);
-          if (obj.healthPoints > 0) {
-            console.log(
-              `${this.name} attacks ${obj.name} with ${
-                this.weapons[randomWeapon]
-              } for ${damageAmount} damage! ${obj.name} has ${
-                obj.healthPoints
-              } health points left!`
-            );
-          } else {
-            console.log(
-              `${this.name} attacks ${obj.name} with ${
-                this.weapons[randomWeapon]
-              } for ${damageAmount}! ${obj.name} has ${
-                obj.healthPoints
-              } health points left!`
-            );
-            console.log(obj.destroy());
-          }
-        }
-      };
+  };
 }
 
 const demon = new Villain({
@@ -199,7 +198,7 @@ const angel = new Hero({
   language: "Demoniac"
 });
 
-window.setInterval(function() {
+let fightInterval = window.setInterval(function() {
   if (angel.healthPoints > 0) {
     angel.justiceStrike(demon);
   }
@@ -207,6 +206,6 @@ window.setInterval(function() {
     demon.sneakAttack(angel);
   }
   if (angel.healthPoints < 1 || demon.healthPoints < 1) {
-    clearInterval();
+    clearInterval(fightInterval);
   }
 }, 1);
